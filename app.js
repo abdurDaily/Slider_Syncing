c41af797c42d82830a8c760af53fe6927321ff0b
abdurDaily <abdurdiary@gmail.com>
2024-01-19 19:43:46 -0800

    slider

---
+++ app.js
@@ -0,0 +1,20 @@
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // arrows: true,
+    fade: true,
+    dots:false,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    arrows:true,
+    centerMode: true,
+    focusOnSelect: true,
+
+
+  
+  });
