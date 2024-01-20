33ca94c90007bfe6aecbadc34886272ba9ca69ff
abdurDaily <abdurdaily@gmail.com>
2024-01-20 12:38:39 +0600

    ok

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    // arrows: true,
+    arrows: false,
     fade: true,
     dots:false,
     asNavFor: '.slider-nav'
   });
   $('.slider-nav').slick({
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     dots: false,
     arrows:true,
     centerMode: true,
+    centerPadding: '0px',
     focusOnSelect: true,
 
 
